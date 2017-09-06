31962be5691d7d165402149726e6ec3e0b9dc3ff
Micah Silverman <micah@afitnerd.com>
2017-09-06 00:47:03 -0400

    simplified install for coveralls

--- scripts/coveralls-maven-snapshot-setup.sh
+++ scripts/coveralls-maven-snapshot-setup.sh
@@ -2,5 +2,5 @@
 
 for i in `ls $TRAVIS_BUILD_DIR/support`;
 do
-  mvn org.apache.maven.plugins:maven-install-plugin:2.5:install-file -Dfile=$TRAVIS_BUILD_DIR/support/$i
+  mvn org.apache.maven.plugins:maven-install-plugin:2.5:install-file -Dfile=$TRAVIS_BUILD_DIR/support/$i -DpomFile=pom.xml
 done
